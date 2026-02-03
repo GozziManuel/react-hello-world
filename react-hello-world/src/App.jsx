cb04d1660a6ecd897f24c92f7674b9c0750dfca2
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-03 17:19:36 +0100

    hello world

--- react-hello-world/src/App.jsx
+++ react-hello-world/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Hello World</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat esse
+        deserunt perspiciatis, aliquid est nihil tempora tenetur id culpa, ad
+        dolorum temporibus dolore minima in nulla, repudiandae sequi hic. Ab?
+        Culpa ipsam consequatur quam quasi blanditiis eum molestiae, ab mollitia
+        assumenda quo maiores nesciunt in nobis. Beatae, voluptas adipisci quas
+        aliquid veritatis quasi ullam alias, eum nisi illo dolor officiis. Minus
+        neque harum, pariatur eligendi molestias delectus eaque architecto
+        corrupti! Dignissimos tempore quaerat voluptatem, molestias ipsa
+        voluptas veritatis corporis tenetur nesciunt ipsam sequi dolor repellat,
+        praesentium incidunt, eveniet assumenda accusantium?
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
